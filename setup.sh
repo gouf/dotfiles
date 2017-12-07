77fbe630945a1edf5411d5662db3aa4f178d21d2
gouf <innocent.zero@gmail.com>
2017-12-08 00:43:41 +0900

    Fix dependencies

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Deploy this file where to own domain, like ubuntu.example.com/setup.sh
 
 sudo apt update && sudo apt upgrade -y \
+  && sudo apt install -y make \
   && curl https://raw.githubusercontent.com/gouf/dotfiles/master/Makefile > Makefile \
   && make \
   && rm Makefile
